2ba68518399c0baaa8fb45055220c9d00b40f6df
Arteom Katkov <arteomk0626@gmail.com>
2024-07-01 22:28:21 -0400

    Add IRQChip driver

--- drivers/irqchip/irq-nuc980.c
+++ drivers/irqchip/irq-nuc980.c
@@ -23,17 +23,138 @@
 #include <linux/ptrace.h>
 #include <linux/device.h>
 #include <linux/io.h>
+#include <linux/of_address.h>
 
 #include <asm/irq.h>
 #include <linux/of.h>
 #include <linux/of_irq.h>
+#include <linux/irqchip.h>
 #include <asm/mach/irq.h>
 
-#include "include/mach/hardware.h"
-#include "include/mach/regs-aic.h"
-#include "include/mach/regs-gpio.h"
-#include "include/mach/gpio.h"
-// #include <asm/gpio.h>
+/* Advance Interrupt Controller (AIC) Registers */
+#define		REG_AIC_SRCCTL0		(0x00)	/* Source control register 0 */
+#define		REG_AIC_SRCCTL1		(0x04)	/* Source control register 1 */
+#define		REG_AIC_SRCCTL2		(0x08)	/* Source control register 2 */
+#define		REG_AIC_SRCCTL3		(0x0C)	/* Source control register 3 */
+#define		REG_AIC_SRCCTL4		(0x10)	/* Source control register 4 */
+#define		REG_AIC_SRCCTL5		(0x14)	/* Source control register 5 */
+#define		REG_AIC_SRCCTL6		(0x18)	/* Source control register 6 */
+#define		REG_AIC_SRCCTL7		(0x1C)	/* Source control register 7 */
+#define		REG_AIC_SRCCTL8		(0x20)	/* Source control register 8 */
+#define		REG_AIC_SRCCTL9		(0x24)	/* Source control register 9 */
+#define		REG_AIC_SRCCTL10	(0x28)	/* Source control register 10 */
+#define		REG_AIC_SRCCTL11	(0x2C)	/* Source control register 11 */
+#define		REG_AIC_SRCCTL12	(0x30)	/* Source control register 12 */
+#define		REG_AIC_SRCCTL13	(0x34)	/* Source control register 13 */
+#define		REG_AIC_SRCCTL14	(0x38)	/* Source control register 14 */
+#define		REG_AIC_SRCCTL15	(0x3C)	/* Source control register 15 */
+#define		REG_AIC_IRQNUM		(0x120)	/* IRQ source number register */
+#define		REG_AIC_FIQNUM		(0x124)	/* FIQ source number register */
+#define		REG_AIC_INTMSK0		(0x128)	/* Interrupt mask register 0 */
+#define		REG_AIC_INTMSK1		(0x12C)	/* Interrupt mask register 1 */
+#define		REG_AIC_INTEN0		(0x130)	/* Mask enable command register 0 */
+#define		REG_AIC_INTEN1		(0x134)	/* Mask enable command register 1 */
+#define		REG_AIC_INTDIS0		(0x138)	/* Mask disable command register 0 */
+#define		REG_AIC_INTDIS1		(0x13C)	/* Mask disable command register 1 */
+#define		REG_AIC_EOIS		(0x150)	/* End of IRQ service command register */
+#define		REG_AIC_EOFS		(0x154)	/* End of FIQ service command register */
+
+#define NUC980_IRQ(x)	(x)
+
+/* Main cpu interrupts */
+
+#define IRQ_WDT		NUC980_IRQ(1)
+#define IRQ_WWDT	NUC980_IRQ(2)
+#define IRQ_LVD		NUC980_IRQ(3)
+#define IRQ_EXT0	NUC980_IRQ(4)
+#define IRQ_EXT1	NUC980_IRQ(5)
+#define IRQ_EXT2	NUC980_IRQ(6)
+#define IRQ_EXT3	NUC980_IRQ(7)
+#define IRQ_GPA		NUC980_IRQ(8)
+#define IRQ_GPB		NUC980_IRQ(9)
+#define IRQ_GPC		NUC980_IRQ(10)
+#define IRQ_GPD		NUC980_IRQ(11)
+#define IRQ_I2S		NUC980_IRQ(12)
+
+#define IRQ_CAP0	NUC980_IRQ(14)
+#define IRQ_RTC		NUC980_IRQ(15)
+#define IRQ_TIMER0	NUC980_IRQ(16)
+#define IRQ_TIMER1	NUC980_IRQ(17)
+#define IRQ_ADC		NUC980_IRQ(18)
+#define IRQ_EMC0RX	NUC980_IRQ(19)
+#define IRQ_EMC1RX	NUC980_IRQ(20)
+#define IRQ_EMC0TX	NUC980_IRQ(21)
+#define IRQ_EMC1TX	NUC980_IRQ(22)
+#define IRQ_EHCI	NUC980_IRQ(23)
+#define IRQ_OHCI	NUC980_IRQ(24)
+#define IRQ_PDMA0	NUC980_IRQ(25)
+#define IRQ_PDMA1	NUC980_IRQ(26)
+#define IRQ_SDH		NUC980_IRQ(27)
+#define IRQ_FMI		NUC980_IRQ(28)
+#define IRQ_UDC		NUC980_IRQ(29)
+#define IRQ_TIMER2	NUC980_IRQ(30)
+#define IRQ_TIMER3	NUC980_IRQ(31)
+#define IRQ_TIMER4	NUC980_IRQ(32)
+#define IRQ_CAP1	NUC980_IRQ(33)
+#define IRQ_TIMER5	NUC980_IRQ(34)
+#define IRQ_CRYPTO	NUC980_IRQ(35)
+#define IRQ_UART0	NUC980_IRQ(36)
+#define IRQ_UART1	NUC980_IRQ(37)
+#define IRQ_UART2	NUC980_IRQ(38)
+#define IRQ_UART4	NUC980_IRQ(39)
+#define IRQ_UART6	NUC980_IRQ(40)
+#define IRQ_UART8	NUC980_IRQ(41)
+#define IRQ_CAN3	NUC980_IRQ(42)
+#define IRQ_UART3	NUC980_IRQ(43)
+#define IRQ_UART5	NUC980_IRQ(44)
+#define IRQ_UART7	NUC980_IRQ(45)
+#define IRQ_UART9	NUC980_IRQ(46)
+#define IRQ_I2C2	NUC980_IRQ(47)
+#define IRQ_I2C3	NUC980_IRQ(48)
+#define IRQ_GPE		NUC980_IRQ(49)
+#define IRQ_SPI2	NUC980_IRQ(50)
+#define IRQ_SPI0	NUC980_IRQ(51)
+#define IRQ_SPI1	NUC980_IRQ(52)
+#define IRQ_I2C0	NUC980_IRQ(53)
+#define IRQ_I2C1	NUC980_IRQ(54)
+#define IRQ_SMC0	NUC980_IRQ(55)
+#define IRQ_SMC1	NUC980_IRQ(56)
+#define IRQ_GPF		NUC980_IRQ(57)
+#define IRQ_CAN0	NUC980_IRQ(58)
+#define IRQ_CAN1	NUC980_IRQ(59)
+#define IRQ_PWM0	NUC980_IRQ(60)
+#define IRQ_PWM1	NUC980_IRQ(61)
+#define IRQ_CAN2	NUC980_IRQ(62)
+#define IRQ_GPG		NUC980_IRQ(63)
+
+#ifndef CONFIG_GPIO_NUC980
+#define SPARE_IRQS	(64)
+#define NOF_IRQS		(IRQ_GPG + SPARE_IRQS + 1)
+
+#else
+#define EXT0_BASE 100
+#define IRQ_EXT0_A0           NUC980_IRQ(EXT0_BASE + 0)
+#define IRQ_EXT1_A1           NUC980_IRQ(EXT0_BASE + 1)
+#define IRQ_EXT2_D0           NUC980_IRQ(EXT0_BASE + 2)
+#define IRQ_EXT3_D1           NUC980_IRQ(EXT0_BASE + 3)
+
+#define IRQ_EXT0_A13          NUC980_IRQ(EXT0_BASE + 4)
+#define IRQ_EXT1_A14          NUC980_IRQ(EXT0_BASE + 5)
+#define IRQ_EXT2_E10          NUC980_IRQ(EXT0_BASE + 6)
+#define IRQ_EXT3_E12          NUC980_IRQ(EXT0_BASE + 7)
+
+#define IRQ_EXT2_B3           NUC980_IRQ(EXT0_BASE + 8)
+#define IRQ_EXT2_B13          NUC980_IRQ(EXT0_BASE + 9)
+#define IRQ_EXT3_G15          NUC980_IRQ(EXT0_BASE +10)
+
+#define IRQ_GPIO_START	NUC980_IRQ(NUC980_IRQ(0x100))
+#define IRQ_GPIO_END		NUC980_IRQ(NUC980_IRQ(0x100 + 0xE0))
+
+#define SPARE_IRQS		(64)
+
+#define NOF_IRQS			(IRQ_GPIO_END + SPARE_IRQS +1)
+
+#endif
 
 #if 0
 #define ENTRY()                 printk("[%-20s] : Enter...\n", __FUNCTION__)
@@ -43,25 +164,27 @@
 #define LEAVE()
 #endif
 
+static void __iomem *reg_base;
+
 static void nuc980_irq_mask(struct irq_data *d)
 {
 	if (d->irq < 32)
-		__raw_writel(1 << (d->irq), REG_AIC_INTDIS0);
+		__raw_writel(1 << (d->irq), reg_base + REG_AIC_INTDIS0);
 	else
-		__raw_writel(1 << (d->irq - 32), REG_AIC_INTDIS1);
+		__raw_writel(1 << (d->irq - 32), reg_base + REG_AIC_INTDIS1);
 }
 
 static void nuc980_irq_ack(struct irq_data *d)
 {
-	__raw_writel(0x01, REG_AIC_EOIS);
+	__raw_writel(0x01, reg_base + REG_AIC_EOIS);
 }
 
 static void nuc980_irq_unmask(struct irq_data *d)
 {
 	if (d->irq < 32)
-		__raw_writel(1 << (d->irq), REG_AIC_INTEN0);
+		__raw_writel(1 << (d->irq), reg_base + REG_AIC_INTEN0);
 	else
-		__raw_writel(1 << (d->irq - 32), REG_AIC_INTEN1);
+		__raw_writel(1 << (d->irq - 32), reg_base + REG_AIC_INTEN1);
 }
 
 static int nuc980_irq_set_wake(struct irq_data *d, unsigned int on)
@@ -241,8 +364,8 @@ static void nuc980_irq_demux_intgroupA(struct irq_desc *desc)
 	unsigned int j, isr;
 	unsigned int flag=0;
 	ENTRY();
-	isr = __raw_readl(REG_GPIOA_INTSRC);
-	__raw_writel(isr,REG_GPIOA_INTSRC);
+	isr = __raw_readl(reg_base + REG_GPIOA_INTSRC);
+	__raw_writel(isr, reg_base + REG_GPIOA_INTSRC);
 	for (j = 0; j < 16; j++) {
 		if (isr  & 0x1) {
 			flag=1;
@@ -260,8 +383,8 @@ static void nuc980_irq_demux_intgroupB(struct irq_desc *desc)
 	unsigned int j, isr;
 	unsigned int flag=0;
 	ENTRY();
-	isr = __raw_readl(REG_GPIOB_INTSRC);
-	__raw_writel(isr,REG_GPIOB_INTSRC);
+	isr = __raw_readl(reg_base + REG_GPIOB_INTSRC);
+	__raw_writel(isr, reg_base + REG_GPIOB_INTSRC);
 	for (j = 0; j < 16; j++) {
 		if (isr  & 0x1) {
 			flag=1;
@@ -279,8 +402,8 @@ static void nuc980_irq_demux_intgroupC(struct irq_desc *desc)
 	unsigned int j, isr;
 	unsigned int flag=0;
 	ENTRY();
-	isr = __raw_readl(REG_GPIOC_INTSRC);
-	__raw_writel(isr,REG_GPIOC_INTSRC);
+	isr = __raw_readl(reg_base + REG_GPIOC_INTSRC);
+	__raw_writel(isr, reg_base + REG_GPIOC_INTSRC);
 	for (j = 0; j < 16; j++) {
 		if (isr  & 0x1) {
 			flag=1;
@@ -298,8 +421,8 @@ static void nuc980_irq_demux_intgroupD(struct irq_desc *desc)
 	unsigned int j, isr;
 	unsigned int flag=0;
 	ENTRY();
-	isr = __raw_readl(REG_GPIOD_INTSRC);
-	__raw_writel(isr,REG_GPIOD_INTSRC);
+	isr = __raw_readl(reg_base + REG_GPIOD_INTSRC);
+	__raw_writel(isr, reg_base + REG_GPIOD_INTSRC);
 	for (j = 0; j < 16; j++) {
 		if (isr  & 0x1) {
 			flag=1;
@@ -317,8 +440,8 @@ static void nuc980_irq_demux_intgroupE(struct irq_desc *desc)
 	unsigned int j, isr;
 	unsigned int flag=0;
 	ENTRY();
-	isr = __raw_readl(REG_GPIOE_INTSRC);
-	__raw_writel(isr,REG_GPIOE_INTSRC);
+	isr = __raw_readl(reg_base + REG_GPIOE_INTSRC);
+	__raw_writel(isr, reg_base + REG_GPIOE_INTSRC);
 	for (j = 0; j < 16; j++) {
 		if (isr  & 0x1) {
 			flag=1;
@@ -336,8 +459,8 @@ static void nuc980_irq_demux_intgroupF(struct irq_desc *desc)
 	unsigned int j, isr;
 	unsigned int flag=0;
 	ENTRY();
-	isr = __raw_readl(REG_GPIOF_INTSRC);
-	__raw_writel(isr,REG_GPIOF_INTSRC);
+	isr = __raw_readl(reg_base + REG_GPIOF_INTSRC);
+	__raw_writel(isr, reg_base + REG_GPIOF_INTSRC);
 	for (j = 0; j < 16; j++) {
 		if (isr  & 0x1) {
 			flag=1;
@@ -355,8 +478,8 @@ static void nuc980_irq_demux_intgroupG(struct irq_desc *desc)
 	unsigned int j, isr;
 	unsigned int flag=0;
 	ENTRY();
-	isr = __raw_readl(REG_GPIOG_INTSRC);
-	__raw_writel(isr,REG_GPIOG_INTSRC);
+	isr = __raw_readl(reg_base + REG_GPIOG_INTSRC);
+	__raw_writel(isr, reg_base + REG_GPIOG_INTSRC);
 	for (j = 0; j < 16; j++) {
 		if (isr  & 0x1) {
 			flag=1;
@@ -390,13 +513,13 @@ static void nuc980_irq_ext_mask(struct irq_data *d)
 {
 	ENTRY();
 	if(d->irq==IRQ_EXT0_A0 || d->irq==IRQ_EXT0_A13)
-		__raw_writel(1<<IRQ_EXT0, REG_AIC_INTDIS0);
+		__raw_writel(1<<IRQ_EXT0, reg_base + REG_AIC_INTDIS0);
 	else if(d->irq==IRQ_EXT1_A1 || d->irq==IRQ_EXT1_A14)
-		__raw_writel(1 <<IRQ_EXT1, REG_AIC_INTDIS0);
+		__raw_writel(1 <<IRQ_EXT1, reg_base + REG_AIC_INTDIS0);
 	else if(d->irq==IRQ_EXT2_D0 || d->irq==IRQ_EXT2_E10 || d->irq==IRQ_EXT2_B3 || d->irq==IRQ_EXT2_B13)
-		__raw_writel(1 <<IRQ_EXT2, REG_AIC_INTDIS0);
+		__raw_writel(1 <<IRQ_EXT2, reg_base + REG_AIC_INTDIS0);
 	else if(d->irq==IRQ_EXT3_D1 || d->irq==IRQ_EXT3_E12 || d->irq==IRQ_EXT3_G15)
-		__raw_writel(1 <<IRQ_EXT3, REG_AIC_INTDIS0);
+		__raw_writel(1 <<IRQ_EXT3, reg_base + REG_AIC_INTDIS0);
 	LEAVE();
 }
 
@@ -411,13 +534,13 @@ static void nuc980_irq_ext_unmask(struct irq_data *d)
 {
 	ENTRY();
 	if(d->irq==IRQ_EXT0_A0 || d->irq==IRQ_EXT0_A13)
-		__raw_writel(1<<IRQ_EXT0, REG_AIC_INTEN0);
+		__raw_writel(1<<IRQ_EXT0, reg_base + REG_AIC_INTEN0);
 	else if(d->irq==IRQ_EXT1_A1 || d->irq==IRQ_EXT1_A14)
-		__raw_writel(1 <<IRQ_EXT1, REG_AIC_INTEN0);
+		__raw_writel(1 <<IRQ_EXT1, reg_base + REG_AIC_INTEN0);
 	else if(d->irq==IRQ_EXT2_D0 || d->irq==IRQ_EXT2_E10 || d->irq==IRQ_EXT2_B3 || d->irq==IRQ_EXT2_B13)
-		__raw_writel(1 <<IRQ_EXT2, REG_AIC_INTEN0);
+		__raw_writel(1 <<IRQ_EXT2, reg_base + REG_AIC_INTEN0);
 	else if(d->irq==IRQ_EXT3_D1 || d->irq==IRQ_EXT3_E12 || d->irq==IRQ_EXT3_G15)
-		__raw_writel(1 <<IRQ_EXT3, REG_AIC_INTEN0);
+		__raw_writel(1 <<IRQ_EXT3, reg_base + REG_AIC_INTEN0);
 	LEAVE();
 }
 
@@ -501,7 +624,7 @@ static void nuc980_irq_demux_intgroup2(struct irq_desc *desc)
 			             (volatile unsigned int *)(Port[port1] +
 			                                       0x20));
 		} else
-			__raw_writel(0x01, REG_AIC_EOIS);
+			__raw_writel(0x01, reg_base + REG_AIC_EOIS);
 		break;
 	case IRQ_EXT1:
 		if (__raw_readl((volatile unsigned int *)(Port[port0] + 0x20)) &
@@ -518,7 +641,7 @@ static void nuc980_irq_demux_intgroup2(struct irq_desc *desc)
 			             (volatile unsigned int *)(Port[port1] +
 			                                       0x20));
 		} else
-			__raw_writel(0x01, REG_AIC_EOIS);
+			__raw_writel(0x01, reg_base + REG_AIC_EOIS);
 		break;
 	case IRQ_EXT2:
 		port2 = EXT[8] / GPIO_OFFSET;
@@ -553,7 +676,7 @@ static void nuc980_irq_demux_intgroup2(struct irq_desc *desc)
 			             (volatile unsigned int *)(Port[port3] +
 			                                       0x20));
 		} else
-			__raw_writel(0x01, REG_AIC_EOIS);
+			__raw_writel(0x01, reg_base + REG_AIC_EOIS);
 		break;
 	case IRQ_EXT3:
 		port2 = EXT[10] / GPIO_OFFSET;
@@ -579,84 +702,21 @@ static void nuc980_irq_demux_intgroup2(struct irq_desc *desc)
 			             (volatile unsigned int *)(Port[port2] +
 			                                       0x20));
 		} else
-			__raw_writel(0x01, REG_AIC_EOIS);
+			__raw_writel(0x01, reg_base + REG_AIC_EOIS);
 		break;
 	}
 	LEAVE();
 }
 #endif
 
-void __init nuc980_init_irq(void)
-{
-#if !defined(CONFIG_USE_OF)
-
-	int irqno;
-
-	__raw_writel(0xFFFFFFFC, REG_AIC_INTDIS0);
-	__raw_writel(0xFFFFFFFF, REG_AIC_INTDIS1);
-
-	for (irqno = IRQ_WDT; irqno < NR_IRQS - SPARE_IRQS; irqno++) {
-		irq_set_chip_and_handler(irqno, &nuc980_irq_chip,
-		                         handle_level_irq);
-		irq_clear_status_flags(irqno, IRQ_NOREQUEST);
-	}
-
-#if defined(CONFIG_GPIO_NUC980)
-	ENTRY();
-	/*
-	 * Install handler for GPIO edge detect interrupts
-	 */
-	irq_set_chip(IRQ_GPA, &nuc980_irq_chip);
-	irq_set_chip(IRQ_GPB, &nuc980_irq_chip);
-	irq_set_chip(IRQ_GPC, &nuc980_irq_chip);
-	irq_set_chip(IRQ_GPD, &nuc980_irq_chip);
-	irq_set_chip(IRQ_GPE, &nuc980_irq_chip);
-	irq_set_chip(IRQ_GPF, &nuc980_irq_chip);
-	irq_set_chip(IRQ_GPG, &nuc980_irq_chip);
-	irq_set_chained_handler(IRQ_GPA, nuc980_irq_demux_intgroupA);
-	irq_set_chained_handler(IRQ_GPB, nuc980_irq_demux_intgroupB);
-	irq_set_chained_handler(IRQ_GPC, nuc980_irq_demux_intgroupC);
-	irq_set_chained_handler(IRQ_GPD, nuc980_irq_demux_intgroupD);
-	irq_set_chained_handler(IRQ_GPE, nuc980_irq_demux_intgroupE);
-	irq_set_chained_handler(IRQ_GPF, nuc980_irq_demux_intgroupF);
-	irq_set_chained_handler(IRQ_GPG, nuc980_irq_demux_intgroupG);
-
-
-	for (irqno = IRQ_GPIO_START; irqno < IRQ_GPIO_END; irqno++) {
-		irq_set_chip_and_handler(irqno, &nuc980_irq_gpio,
-		                         handle_level_irq);
-		irq_clear_status_flags(irqno, IRQ_NOREQUEST);
-	}
-
-	/*
-	 * Install handler for GPIO external interrupts
-	 */
-	for (irqno = IRQ_EXT0; irqno <= IRQ_EXT3; irqno++) {
-		//printk("registering irq %d (extended nuc980 irq)\n", irqno);
-		irq_set_chip(irqno, &nuc980_irq_chip);
-		irq_set_chained_handler(irqno, nuc980_irq_demux_intgroup2);
-	}
-
-	for (irqno = IRQ_EXT0_A0; irqno <= IRQ_EXT3_G15; irqno++) {
-		irq_set_chip_and_handler(irqno, &nuc980_irq_ext,
-		                         handle_level_irq);
-		irq_clear_status_flags(irqno, IRQ_NOREQUEST);
-	}
-	LEAVE();
-#endif
-#endif
-}
-
-#ifdef CONFIG_USE_OF
-
 static struct irq_domain *nuc980_aic_domain;
 
 static int nuc980_aic_irq_map(struct irq_domain *h, unsigned int virq,
                               irq_hw_number_t hw)
 {
-	//printk("nuc980_aic_irq_map: %d %d\n", virq, (int)hw);
+	// printk("nuc980_aic_irq_map: %d %d\n", virq, (int)hw);
 
-	if ((IRQ_WDT <= hw) && (hw < NR_IRQS - SPARE_IRQS)) {
+	if ((IRQ_WDT <= hw) && (hw < NOF_IRQS - SPARE_IRQS)) {
 		irq_set_chip_and_handler(virq, &nuc980_irq_chip,
 		                         handle_level_irq);
 		irq_clear_status_flags(virq, IRQ_NOREQUEST);
@@ -719,7 +779,7 @@ static int nuc980_aic_irq_domain_xlate(struct irq_domain *d,
 {
 	if (WARN_ON(intsize < 2))
 		return -EINVAL;
-	if (WARN_ON(intspec[0] >= NR_IRQS))
+	if (WARN_ON(intspec[0] >= NOF_IRQS))
 		return -EINVAL;
 
 	*out_hwirq = intspec[0];
@@ -733,23 +793,32 @@ static struct irq_domain_ops nuc980_aic_irq_ops = {
 	.xlate = nuc980_aic_irq_domain_xlate,
 };
 
-int __init nuc980_of_init_irq(struct device_node *node,
-                              struct device_node *parent)
+static int __init nuc980_of_init_irq(struct device_node *node, struct device_node *parent)
 {
-	nuc980_aic_domain = irq_domain_add_linear(node, SPARE_IRQS,
-	                    &nuc980_aic_irq_ops, NULL);
+    struct resource res;
+
+	nuc980_aic_domain = irq_domain_add_linear(node, SPARE_IRQS, &nuc980_aic_irq_ops, NULL);
 	if (!nuc980_aic_domain)
 		panic("Failed to add irq domain!!\n");
 
 	irq_set_default_host(nuc980_aic_domain);
 
-	__raw_writel(0xFFFFFFFC, REG_AIC_INTDIS0);
-	__raw_writel(0xFFFFFFFF, REG_AIC_INTDIS1);
+    // Retrieve memory resource from device tree
+    if(of_address_to_resource(node, 0, &res)) {
+        pr_err("Failed to get memory resource\n");
+        return -ENODEV;
+    }
 
-	irq_set_chip_and_handler(IRQ_TIMER4, &nuc980_irq_chip, handle_level_irq);
-	irq_clear_status_flags(IRQ_TIMER4, IRQ_NOREQUEST);
+    // Request memory region
+    reg_base = ioremap(res.start, resource_size(&res));
+    if (!reg_base) {
+        pr_err("Failed to remap memory\n");
+        return -ENOMEM;
+    }
+
+	__raw_writel(0xFFFFFFFC, reg_base + REG_AIC_INTDIS0);
+	__raw_writel(0xFFFFFFFF, reg_base + REG_AIC_INTDIS1);
 
 	return 0;
 }
-
-#endif
+IRQCHIP_DECLARE(irq_nuc980, "nuvoton,nuc980-aic", nuc980_of_init_irq);
